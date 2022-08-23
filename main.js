10a1731fcd17b654686f86aefe3e714fe767ffb3
SHAIKIDREES <86823800+SHAIKIDREES@users.noreply.github.com>
2022-08-23 14:31:42 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function speak(){
     var synth= window.speechSynthesis;
     speak_data_1="The first prediction is "+ prediction_1;
     speak_data_2="And the second prediction is "+prediction_2;
-    var utterThis = new SpeechSynthesisUtterrance(speak_data_1+speak_data_2);
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
     synth.speak(utterThis);
 
         
